3281b2e829c5ac4298c7b274ab625679bb18d045
Sang Kibet Elkana <elkanahkibet6@gmail.com>
2023-03-16 16:26:44 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const drivers = ['Elkana', 'Kibet', 'Sang', 'Victor'];
+const returnFirstTwoDrivers = (drivers)=>([drivers[0],drivers[1]])
+
+//const returnFirstTwoDrivers = (drivers)=>{ return [drivers[0],drivers[1]]}
+
+const returnLastTwoDrivers= (drivers)=>([drivers[2],drivers[3]])
+
+
+const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers];
+
+
+//create a function that returns a function
+function createFareMultiplier(myDestin){
+    return  function(amountPaid){
+        return myDestin*amountPaid;
+    }
+
+}
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers =(arrayOfDrivers,anyTwoDriver)=>anyTwoDriver(arrayOfDrivers);
